b88e41cdf0e23112ede7ad19beb0b9729805b885
Govind Kashyap <govind700789@gmail.com>
2026-04-24 22:46:10 +0530

    fixed dotenv in db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,9 @@
 import mongoose from "mongoose";
 import dns from "node:dns/promises";
+import dotenv from "dotenv"
 dns.setServers(["1.1.1.1", "1.0.0.1"]);
 
+dotenv.config()
 const connectDB = () => { 
   mongoose.connect(process.env.MONGO_URI)
   .then(() => {
