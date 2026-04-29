885fa57714388f026a48015887d1f2907eb069fe
Govind Kashyap <govind700789@gmail.com>
2026-04-29 20:35:25 +0530

    applied retry logic to mongodb

--- src/config/db.js
+++ src/config/db.js
@@ -4,14 +4,17 @@ import dotenv from "dotenv"
 dns.setServers(["1.1.1.1", "1.0.0.1"]);
 
 dotenv.config()
-const connectDB = () => { 
-  mongoose.connect(process.env.MONGO_URI)
-  .then(() => {
+const connectDB = async () => { 
+  try{
+    await mongoose.connect(process.env.MONGO_URI)
     console.log("MongoDB connected successfully");
-  })
-  .catch((err) => {
+  }catch{
     console.error("MongoDB connection error:", err.message);
-  });
+    setTimeout(connectDB, 5000);
+  }
+  
+    
+    
 
 }
 export default connectDB;
